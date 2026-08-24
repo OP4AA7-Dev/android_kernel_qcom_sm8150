b9ea409a06cde893df016e7698eaf2da3090d373
Ying-Chen <2132703527@qq.com>
2026-08-24 14:13:54 +0800

    soc: qcom: Import oplus subsystem_restart changes

--- drivers/soc/qcom/subsystem_restart.c
+++ drivers/soc/qcom/subsystem_restart.c
@@ -1827,6 +1827,9 @@ struct subsys_device *subsys_register(struct subsys_desc *desc)
 	subsys->dev.bus = &subsys_bus_type;
 	subsys->dev.release = subsys_device_release;
 	subsys->notif_state = -1;
+	#ifdef CONFIG_MACH_OPLUS_SM7150
+	subsys->restart_level = RESET_SUBSYS_COUPLED;
+	#endif
 	subsys->desc->sysmon_pid = -1;
 	subsys->desc->state = NULL;
 	strlcpy(subsys->desc->fw_name, desc->name,
