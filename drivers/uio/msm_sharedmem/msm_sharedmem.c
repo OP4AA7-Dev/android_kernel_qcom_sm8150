3b81b5f29f46605439d6fca616d8fae166b28454
Ying-Chen <2132703527@qq.com>
2026-08-24 13:54:49 +0800

    uio: Import oembackup_sharedmem changes  - For oplus oembackup (fix IMEI)

--- drivers/uio/msm_sharedmem/msm_sharedmem.c
+++ drivers/uio/msm_sharedmem/msm_sharedmem.c
@@ -25,6 +25,10 @@
 #define CLIENT_ID_PROP "qcom,client-id"
 #define MPSS_RMTS_CLIENT_ID 1
 
+#ifdef CONFIG_MACH_OPLUS_SM7150
+#define MPSS_OEMBACK_CLIENT_ID 4
+#endif
+
 static int uio_get_mem_index(struct uio_info *info, struct vm_area_struct *vma)
 {
 	if (vma->vm_pgoff >= MAX_UIO_MAPS)
@@ -81,7 +85,11 @@ static void setup_shared_ram_perms(u32 client_id, phys_addr_t addr, u32 size,
 	int ret;
 	u32 source_vmlist[1] = {VMID_HLOS};
 
+	#ifndef CONFIG_MACH_OPLUS_SM7150
 	if (client_id != MPSS_RMTS_CLIENT_ID)
+	#else
+	if ((client_id != MPSS_RMTS_CLIENT_ID) && (client_id != MPSS_OEMBACK_CLIENT_ID))
+	#endif
 		return;
 
 	if (vm_nav_path) {
